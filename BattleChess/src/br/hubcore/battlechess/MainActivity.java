ff73581ed1ca5a343bf61a47a0d700ceb2b63f2a
amanajas <user7l44@gmail.com>
2013-06-25 15:47:09 -0300

    Projeto base para construção do CHESS

---
+++ BattleChess/src/br/hubcore/battlechess/MainActivity.java
@@ -0,0 +1,22 @@
+package br.hubcore.battlechess;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+
+    }
+}
